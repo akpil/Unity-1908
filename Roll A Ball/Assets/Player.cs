fe0ff336a5163b96d7381b304064437e6fda52d9
Akpil <jyk0802@naver.com>
2019-10-15 19:04:04 +0900

    프로젝트 생성, 에디터 조작, execution order

---
+++ Roll A Ball/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("On enable");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
