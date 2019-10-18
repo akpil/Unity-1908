959e0ba20c4ea8b23813b9927102ac318458b824
Akpil <jyk0802@naver.com>
2019-10-18 19:52:30 +0900

    기본 설정, 라이트, 플레이어 이동

---
+++ Shooting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float mSpeed, mTilt;
+
+    [SerializeField]
+    private float mXMax, mXMin, mZMax, mZMin;
+
+    private Rigidbody mRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        mRB.velocity = new Vector3(horizontal, 0, vertical).normalized * mSpeed;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, mXMin, mXMax),
+                                         transform.position.y,
+                                         Mathf.Clamp(transform.position.z, mZMin, mZMax));
+        transform.rotation = Quaternion.Euler(0, 0, mTilt * -horizontal);
+
+    }
+}
