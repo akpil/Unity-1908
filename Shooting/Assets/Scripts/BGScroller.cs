dd996715f90aedc99f84f1b65c96f7c0d7eb6ba7
Akpil <jyk0802@naver.com>
2019-10-24 19:52:50 +0900

    BGScroller, enemy setup

---
+++ Shooting/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float mSpeed;
+    private Rigidbody mRB;
+    private Vector3 mMoveDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+        mRB.velocity = Vector3.back * mSpeed;
+        mMoveDistance = new Vector3(0, 0, 20.47f * 2);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bumper"))
+        {
+            transform.position += mMoveDistance;
+        }
+    }
+}
---
+++ Shooting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float mSpeed;
+    private Rigidbody mRB;
+
+    private void Awake()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        mRB.velocity = Vector3.back * mSpeed;
+    }
+
+    
+}
