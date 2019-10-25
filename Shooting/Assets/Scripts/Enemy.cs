08f1bb83df848a2675eb9416cc1603756564df3d
Akpil <jyk0802@naver.com>
2019-10-25 19:53:07 +0900

    enemy movement , enemy auto fire

--- Shooting/Assets/Scripts/Enemy.cs
+++ Shooting/Assets/Scripts/Enemy.cs
@@ -8,15 +8,61 @@ public class Enemy : MonoBehaviour
     private float mSpeed;
     private Rigidbody mRB;
 
+    [SerializeField]
+    private BoltPool mBoltPool;
+    [SerializeField]
+    private Transform mBoltPos;
     private void Awake()
     {
-        mRB = GetComponent<Rigidbody>();
+        mRB = GetComponent<Rigidbody>();        
     }
 
     private void OnEnable()
     {
         mRB.velocity = Vector3.back * mSpeed;
+        StartCoroutine(MovePattern());
+        StartCoroutine(AutoFire());
     }
 
-    
+    private IEnumerator AutoFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.6f);
+            Bolt newBolt = mBoltPool.GetFromPool();
+            newBolt.transform.position = mBoltPos.position;
+            newBolt.transform.rotation = mBoltPos.rotation;
+
+        }
+    }
+
+    private IEnumerator MovePattern()
+    {
+        while (true)
+        {
+
+            // vel = (0,0,mSpeed)
+            yield return new WaitForSeconds(Random.Range(.5f, 1.5f));
+            if (transform.position.x < 0)
+            {
+                mRB.velocity += Vector3.right * Random.Range(2, 5f);
+            }
+            else
+            {
+                mRB.velocity += Vector3.left * Random.Range(2, 5f);
+            }
+            // vel = (x, 0, mSpeed)
+            yield return new WaitForSeconds(Random.Range(.5f, 1.5f));
+
+            Vector3 oriVel = mRB.velocity; // oriVel = (x, 0, mSpeed)
+            oriVel.x = 0; // orivel = (0,0,mSpeed)
+            mRB.velocity = oriVel;
+
+            //// (x, 0, mSpeed) -= (x, 0, 0) 
+            //mRB.velocity -= new Vector3(mRB.velocity.x, 0, 0);
+            //// (0, 0, mSpeed)
+            //mRB.velocity = new Vector3(0, 0, mRB.velocity.z);
+        }
+    }
 }
+
