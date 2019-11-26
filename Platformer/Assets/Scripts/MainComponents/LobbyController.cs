5391c3e2644204076994771b59a04216bbb8e5d6
Akpil <jyk0802@naver.com>
2019-11-26 19:39:52 +0900

    MoveFiles, Button OnClick

---
+++ Platformer/Assets/Scripts/MainComponents/LobbyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            StartMainGame();
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/UIComponents/LobbyUIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUIController : MonoBehaviour
+{
+    delegate void AAA();
+    AAA testA;
+    Dictionary<string, AAA> mDic;
+    [SerializeField]
+    private Button mStartMainGameButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mStartMainGameButton.onClick.AddListener(() => { Debug.Log("sfdsfsdf"); });
+        mDic = new Dictionary<string, AAA>();
+        mStartMainGameButton.onClick.AddListener(Test);
+        SetUP(5);
+        SetUP(7);
+        mStartMainGameButton.onClick.RemoveListener(Test);
+        mStartMainGameButton.onClick.Invoke(); //OnClick강제실행
+    }
+
+    private void Test()
+    {
+        Debug.Log("Test");
+    }
+
+    private void SetUP(int Data)
+    {
+        mStartMainGameButton.onClick.AddListener(() => { Debug.Log(Data.ToString()); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
