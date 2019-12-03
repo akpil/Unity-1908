19bc05a6d41f5373a5f9cb8c00d86bcf3103bf0a
Akpil <jyk0802@naver.com>
2019-12-03 19:50:41 +0900

    Clicker setup

---
+++ Clicker/Assets/Scripts/TitleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField]
+    private Button mStartButton;
+    [SerializeField]
+    private Text mStatusText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mStartButton.onClick.AddListener(StartMainGame);
+        mStatusText.text = "Touch to Start";
+    }
+
+    public void StartMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
