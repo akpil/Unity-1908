5653a20840dfe7ff4f5a296472d2f20e803b38d1
Akpil <jyk0802@naver.com>
2019-12-04 19:50:46 +0900

    Scrollwindow move anim

---
+++ Clicker/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIController : MonoBehaviour
+{
+    private static int mUIMoveHash = Animator.StringToHash("Move");
+    [SerializeField]
+    Animator[] mWindowAnims;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MoveWindow(int id)
+    {
+        mWindowAnims[id].SetTrigger(mUIMoveHash);
+    }
+}
